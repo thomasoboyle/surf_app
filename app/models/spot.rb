5b8d1c41ff851af7d52e82127f1acddb2dd6c32e
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-11-09 19:07:22 -0800

    Brings work on creation validation

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,7 @@
 class Spot < ApplicationRecord
+  validates :title,     presence: true,
+                        length: { minimum: 5,
+                        too_short: "#{:title} must be at least %{count} characters long" }
+  validates :location,  presence: true,
+                        length: { minimum: 5 }
 end
