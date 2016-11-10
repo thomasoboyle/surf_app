8862245eb04a4493028b21f73ec8d9b09024e32b
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-11-09 18:23:47 -0800

    Added links to the index and create pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    render html: "Welcome to the surf app! More cool surf coming soon"
+    render html: "Welcome to the surf app! More cool surf stuff coming soon"
   end
 end
