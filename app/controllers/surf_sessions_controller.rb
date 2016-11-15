f5782c1722bd3f48eebd3b164ed22c05d41ddccf
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-11-14 22:31:03 -0800

    Fixes surf session controller

--- app/controllers/surf_sessions_controller.rb
+++ app/controllers/surf_sessions_controller.rb
@@ -1,4 +1,10 @@
 class SurfSessionsController < ApplicationController
   def new
+    @surf_session = SurfSession.new
   end
+
+  def create
+    render plain: params[:surf_session].inspect
+  end
+
 end
