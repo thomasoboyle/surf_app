5b68d33b4b5d467c427bae5ea89fe129672624cf
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-11-15 19:31:35 -0800

    Adds the ability to create a surf session

--- app/controllers/surf_sessions_controller.rb
+++ app/controllers/surf_sessions_controller.rb
@@ -3,8 +3,24 @@ class SurfSessionsController < ApplicationController
     @surf_session = SurfSession.new
   end
 
+  def show
+    @surf_session = SurfSession.find(params[:id])
+  end
+
   def create
-    render plain: params[:surf_session].inspect
+    @surf_session = SurfSession.new(surf_params)
+
+     if @surf_session.save
+      redirect_to @surf_session
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def surf_params
+    params.require(:surf_session).permit(:date, :start_time, :end_time, :average_wave_height, :session_summary, :spot_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
   resources :spots
 
   resources :surf_sessions
-  
-
-
-  get '/spots', to: 'spots#index'
 
   root 'application#index'
 
