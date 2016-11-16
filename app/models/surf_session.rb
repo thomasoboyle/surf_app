c07651bc93e4ce7f4e13552455dd486f0fee3ba0
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-11-15 22:16:10 -0800

    Adds surf session creation validation. All fields must now be present

--- app/models/surf_session.rb
+++ app/models/surf_session.rb
@@ -1,3 +1,10 @@
 class SurfSession < ApplicationRecord
   belongs_to :spot
+   validates :date,
+            :start_time,
+            :end_time,
+            :average_wave_height,
+            :session_summary,
+            :spot_id,             presence: true
+
 end
