c7fedb02b089cb1cc9ab04c7ae2b81735a57cb79
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-11-25 17:56:14 -0800

    Adds the ability to delete a comment from a surf session

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
     redirect_to surf_session_path(@surf_session)
   end
 
+  def destroy
+    @surf_session = SurfSession.find(params[:surf_session_id])
+    @comment = @surf_session.comments.find(params[:id])
+    @comment.destroy
+    redirect_to surf_session_path(@surf_session)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
