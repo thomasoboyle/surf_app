f714b254481501a5a1bd7278600ba85981f2c627
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-11-27 21:52:39 -0800

    Adds validation to comments, IU yet to come

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :surf_session
+  validates :commenter, presence: true,
+                        length: { minimum: 2 }
+  validates :body,      presence: true,
+                        length: { minimum: 5 }
 end
