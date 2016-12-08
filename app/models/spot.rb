b3f6ac49f1fa248f3526a2d22195a131d6e569d2
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-12-07 18:28:17 -0800

    Spot model code clean up

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -7,15 +7,11 @@ class Spot < ApplicationRecord
                         too_short: "#{:title} must be at least %{count} characters long" }
   validates :location,  presence: true,
                         length: { minimum: 2 }
+
+
   def parent_title
-    # initialize parentTitle to "root"
-    title = ""
-    #  if we have a parent
-    if self.spot_id
-      # find the parent
-      parent = Spot.find(self.spot_id)
-      # read its parentTitle and assign it to @parentName
-      title = parent.title
+    if self
+      self.title
     end
   end
 
