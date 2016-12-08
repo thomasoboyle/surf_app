58945b16d010dddb171a5ba63a67ae822d86a80b
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-12-07 19:38:29 -0800

    Adds parents to spots index

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -7,12 +7,17 @@ class Spot < ApplicationRecord
                         too_short: "#{:title} must be at least %{count} characters long" }
   validates :location,  presence: true,
                         length: { minimum: 2 }
-
-
   def parent_title
-    if self
-      self.title
+    # initialize parentTitle to "root"
+    title = ""
+    #  if we have a parent
+    if self.spot_id
+      # find the parent
+      parent = Spot.find(self.spot_id)
+      # read its parentTitle and assign it to @parentName
+      title = parent.title
     end
   end
 
 end
+
