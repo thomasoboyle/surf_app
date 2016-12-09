1f4bf01c41379bacc165ee4d639c84eea0e24b7c
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-12-08 19:35:44 -0800

    Adds text

--- app/models/surf_session.rb
+++ app/models/surf_session.rb
@@ -12,6 +12,7 @@
 #  created_at          :datetime         not null
 #  updated_at          :datetime         not null
 #  surfer              :string
+#  height              :text
 #
 
 class SurfSession < ApplicationRecord
