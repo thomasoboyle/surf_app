9f570de1659f9612a9d3c87c96fc5baad32c8298
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-12-14 21:49:11 -0800

    Reruns migration with default set to zero and null false

--- db/migrate/20161215040119_add_consistency_to_surf_sessions.rb
+++ db/migrate/20161215040119_add_consistency_to_surf_sessions.rb
@@ -1,5 +1,5 @@
 class AddConsistencyToSurfSessions < ActiveRecord::Migration[5.0]
   def change
-    add_column :surf_sessions, :consistency, :integer
+    add_column :surf_sessions, :consistency, :integer, default: 0, null: false
   end
 end
