4810bf1479cbc039cb7f38a567e12ac36960775e
Thomas O'Boyle <thomashoboyle@gmail.com>
2017-03-14 19:40:53 -0700

    Adds validations to new user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,13 +20,13 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).
-      permit(:name, 
-        :email, 
-        :password, 
-        :password_confirmation
-        )
-  end
+    def user_params
+      params.require(:user).
+        permit(:name, 
+          :email, 
+          :password, 
+          :password_confirmation
+          )
+    end
 
 end
