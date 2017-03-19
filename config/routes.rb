0363553a83815e3c4f53c32e3f280444bd918811
Thomas O'Boyle <thomashoboyle@gmail.com>
2017-03-18 22:19:52 -0700

    Adds sessions controller and cleans up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,20 @@
 Rails.application.routes.draw do
 
-  root 'surf_sessions#index'
 
-  get  '/sign_up', to: 'users#new'
+  root 'surf_sessions#index'
 
   resources :surf_sessions do
     resources :comments
   end
 
   resources :tags
-  
   resources :spots
-
   resources :attachments
-
   resources :users
-
-
+  
+  get        '/sign_up', to: 'users#new'
+  get        '/login',   to: 'sessions#new'
+  post       '/login',   to: 'sessions#create'
+  delete     '/logout',  to: 'sessions#destroy'
 
 end
