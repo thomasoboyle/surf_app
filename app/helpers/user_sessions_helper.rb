7e83482ed42e4d581d9b64a9ebd855650f8072c2
Thomas O'Boyle <thomashoboyle@gmail.com>
2017-03-19 17:18:14 -0700

    Adds checks to see if a user is logged in to the user sessions helper

--- app/helpers/user_sessions_helper.rb
+++ app/helpers/user_sessions_helper.rb
@@ -4,4 +4,13 @@ module UserSessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # returns the current logged-in user (if any)
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
