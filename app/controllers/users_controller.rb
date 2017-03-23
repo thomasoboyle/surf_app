97fa616d14a771791336e3d4a932d260cc152be9
Thomas O'Boyle <thomashoboyle@gmail.com>
2017-03-23 15:49:38 -0700

    Adds a users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
@@ -23,9 +26,9 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).
-        permit(:name, 
-          :email, 
-          :password, 
+        permit(:name,
+          :email,
+          :password,
           :password_confirmation
           )
     end
