171d1530b570f147c2706bada72638f51487c9f5
Thomas O'Boyle <thomashoboyle@gmail.com>
2017-04-08 11:52:02 -0700

    Restricts editing users information to those who are logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  #before_action :logged_in_user, only: [:edit, :update]
   def index
     @users = User.all
   end
@@ -47,4 +48,10 @@ class UsersController < ApplicationController
           )
     end
 
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
