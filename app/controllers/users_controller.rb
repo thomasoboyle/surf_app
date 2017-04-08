81d574af1c7036300f6e089845e2a78dc9f4ea68
Thomas O'Boyle <thomashoboyle@gmail.com>
2017-04-08 12:08:27 -0700

    Users are now only able to edit their own profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  #before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   def index
     @users = User.all
   end
@@ -53,5 +54,10 @@ class UsersController < ApplicationController
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(users_path) unless @user == current_user
     end
 end
