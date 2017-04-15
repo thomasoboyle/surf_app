d4fc7a17078b8db1f8faf80e5d6d214f75b4a2d4
Thomas O'Boyle <thomashoboyle@gmail.com>
2017-04-15 15:44:09 -0700

    Admim user test

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
 
   def admin_user?
-    #@current_user ||= User.find_by(id: session[:user_id])
-    #current_user.admin?
+    current_user ||= User.find_by(id: session[:user_id])
+    current_user.admin?
   end
 end
 
