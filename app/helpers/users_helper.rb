683fa28d3d87a34079552754d4567f2de7222cb8
Thomas O'Boyle <thomashoboyle@gmail.com>
2017-04-15 15:36:38 -0700

    Admin user patch

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,14 @@
 module UsersHelper
 
   def admin_user?
-    @current_user ||= User.find_by(id: session[:user_id])
-    current_user.admin?
+    #@current_user ||= User.find_by(id: session[:user_id])
+    #current_user.admin?
   end
 end
+
+    ## I am not sure but there seems to be something wrong
+    ## with current_user. Maybe current users doesnt return
+    ## the full users, which is why I can't call 'admin?'
+    ## on it.
+
+    ## I only experience the error when logged in
