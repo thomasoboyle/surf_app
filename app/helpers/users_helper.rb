009236a6d3f21e31daf80566834172f2ec409f72
Thomas O'Boyle <thomashoboyle@gmail.com>
2017-04-15 22:06:36 -0700

    Adds desired logic

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,8 @@ module UsersHelper
     current_user ||= User.find_by(id: session[:user_id])
     current_user.admin?
   end
+
+  def current_user?
+    current_user ||= User.find_by(id: session[:user_id])
+  end
 end
