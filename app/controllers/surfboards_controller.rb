6933c2e959c358abd3829357e7bf8df8e40be329
Thomas O'Boyle <thomashoboyle@gmail.com>
2017-08-05 13:48:52 -0700

    Add log in check to surfboard actions

--- app/controllers/surfboards_controller.rb
+++ app/controllers/surfboards_controller.rb
@@ -1,4 +1,6 @@
 class SurfboardsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :edit, :delete]
+
   def create
     @user = User.find(params[:user_id])
     @surfboard = @user.surfboards.create(surfboard_params)
@@ -31,5 +33,12 @@ class SurfboardsController < ApplicationController
                   :surfboard_length,
                   :surfboard_type,
                   :surfboard_rating)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = 'Please log in.'
+      redirect_to login_url
+    end
   end
 end
