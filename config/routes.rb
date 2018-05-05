a334689e45c970e532f80d62646da53f6124dc7e
Thomas O'Boyle <thomashoboyle@gmail.com>
2018-05-04 17:35:47 -0700

    Ran rails app:upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,3 @@
 Rails.application.routes.draw do
-  root 'surf_sessions#index'
-
-  resources :surf_sessions do
-    resources :comments
-  end
-
-  resources :tags
-  resources :spots
-  resources :attachments
-  resources :users do
-    resources :surfboards
-  end
-
-  get        '/sign_up', to: 'users#new'
-  get        '/login',   to: 'user_sessions#new'
-  post       '/login',   to: 'user_sessions#create'
-  delete     '/logout',  to: 'user_sessions#destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
