368b6c4a55c85c4e49dfd68f92812f88ddc1f1ed
Thomas O'Boyle <thomashoboyle@gmail.com>
2018-05-07 21:49:50 -0700

    Simple user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -16,17 +16,31 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Test User", email: "test@xample.com", password: "password")
+  end
 
-  test "should not save user without name" do
-    user = User.new
-    assert_not user.save, "Saved the user without a name"
+  test "valid user" do
+    assert @user.valid?
+  end
+
+  test "invalid without email" do
+    @user.email = nil
+    refute @user.valid?
+  end
+
+  test "invalid without password" do
+    @user.password = nil
+    refute @user.valid?
+  end
+
+  test 'invalid without name' do
+    @user.name = nil
+    refute @user.valid?
   end
 
   test "should save user" do
-    user = User.new(name: "Test User", email: "test@xample.com", password: "password")
-    assert user.save, "Saved the user"
+    @user
+    assert @user.save, "Saved the user"
   end
 end
