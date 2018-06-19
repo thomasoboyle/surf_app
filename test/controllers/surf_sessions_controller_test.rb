41483bf7d4a4eaeeccd001be3ec6e523b13de072
Thomas O'Boyle <thomashoboyle@gmail.com>
2018-06-18 20:31:05 -0700

    Surf Session read and new tests. ITL that relationships must be represented in fixtures by calling each other

--- test/controllers/surf_sessions_controller_test.rb
+++ test/controllers/surf_sessions_controller_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class SurfSessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @surf_session = surf_sessions(:surf_session_one)
+  end
+
+  test "should get index" do
+    get surf_sessions_path
+    assert_response :success
+  end
+
+  test "new should redirect without login" do
+    get new_surf_session_path
+    assert_redirected_to login_path
+  end
+
+  test "new should not redirect with login" do
+    user = User.create!(email: "example@example.com", password: "password", name: "example")
+    manual_sign_in_as(user)
+
+    get new_surf_session_path
+    assert_response :success
+  end
+
+  test "should show surf session" do
+    get surf_session_path(@surf_session)
+    assert_response :success
+  end
 end
