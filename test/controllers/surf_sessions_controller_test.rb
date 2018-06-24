cb2d91c9fa3d57e6d17dbd5ffe2d38bd541f884a
Thomas O'Boyle <thomashoboyle@gmail.com>
2018-06-23 21:55:43 -0700

    Update and delete surf spot controller tests

--- test/controllers/surf_sessions_controller_test.rb
+++ test/controllers/surf_sessions_controller_test.rb
@@ -28,4 +28,37 @@ class SurfSessionsControllerTest < ActionDispatch::IntegrationTest
     get surf_session_path(@surf_session)
     assert_response :success
   end
+
+  test "should delete surf session" do
+    surf_session = surf_sessions(:surf_session_one)
+
+    assert_difference('SurfSession.count', -1) do
+      delete surf_session_url(surf_session)
+    end
+  end
+
+  test "should redirect users after surf session deletion" do
+    surf_session = surf_sessions(:surf_session_one)
+
+    delete surf_session_path(surf_session)
+    assert_redirected_to surf_sessions_path
+  end
+
+  test "should update surf session" do
+    user = User.create!(email: "example@example.com", password: "password", name: "example")
+    manual_sign_in_as(user)
+    surf_session = surf_sessions(:two)
+
+    patch surf_session_path(surf_session), params: { surf_session: { session_summary: "updated" } }
+    surf_session.reload
+
+    assert_equal "updated", surf_session.session_summary
+  end
+
+  test "should redirect after update" do
+    surf_session = surf_sessions(:two)
+
+    patch surf_session_path(surf_session), params: { surf_session: { session_summary: "updated" } }
+    assert_redirected_to login_path
+  end
 end
