c828c4b54cbb3d847c6d17dbaa016f4bc79b7d52
Thomas O'Boyle <thomashoboyle@gmail.com>
2018-10-11 21:49:20 -0700

    Render json in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+      constructor(props){
+        super(props)
+        this.state = {
+            data: []
+        }
+    }
+
+  componentDidMount() {
+        axios.get('api/v1/spots.json')
+        .then(response => {
+            console.log(response)
+            this.setState({
+                data: response.data
+            })
+        })
+        .catch(error => console.log(error))
+    }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {JSON.stringify(this.state.data, null, 2)}
       </div>
     );
   }
