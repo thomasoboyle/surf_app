9cc7c3144152402966603e35b06964a9386b0821
Thomas O'Boyle <thomashoboyle@gmail.com>
2018-10-15 21:44:50 -0700

    Add Ant Design

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,47 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import Table from 'antd/lib/table'
+
+const columns = [{
+  title: 'Title',
+  dataIndex: 'title',
+  key: 'title',
+}, {
+  title: 'Location',
+  dataIndex: 'location',
+  key: 'location',
+}, {
+  title: 'Type',
+  dataIndex: 'wave_type',
+  key: 'wave_type',
+}];
 
 class App extends Component {
-      constructor(props){
-        super(props)
-        this.state = {
-            data: []
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
 
   componentDidMount() {
-        axios.get('api/v1/spots.json')
-        .then(response => {
-            console.log(response)
-            this.setState({
-                data: response.data
-            })
-        })
-        .catch(error => console.log(error))
-    }
+    axios
+      .get('api/v1/spots.json')
+      .then(response => {
+        console.log(response);
+        this.setState({
+          data: response.data,
+        });
+      })
+      .catch(error => console.log(error));
+  }
 
   render() {
     return (
       <div>
-        {JSON.stringify(this.state.data, null, 2)}
+        <Table dataSource={this.state.data} columns={columns} />
       </div>
     );
   }
