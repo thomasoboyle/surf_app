3c6377513c587d9a97843dd10147533b9c9587c7
Thomas O'Boyle <thomashoboyle@gmail.com>
2018-10-28 21:25:03 -0700

    Add factorybot configuration to helper (allowing for factorybot)

--- spec/requests/spots_spec.rb
+++ spec/requests/spots_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Spots', type: :request do
+  before do
+    #allow(UserSessionsHelper).to receive(loggined_in?).and_return true
+  end
+  it 'renders the index page' do
+    get '/spots'
+    expect(response).to have_http_status(200)
+  end
   it 'renders the new page' do
+    allow(UserSessionsController).to receive(:logged_in_user).and_return true
     get '/spots/new'
     expect(response).to have_http_status(200)
   end
